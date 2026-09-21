840622233f62007febb9c9fbfb64eafaa3c4c4e0
Siri-AnnNoroff <sirdoe07441@stud.noroff.no>
2026-09-21 13:51:09 +0200

    setup
    set up html, css and js with basic markup,

---
+++ components/composites/siri-modals/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//getting components
+
+const deleteBtn = document.querySelector("deleteBtn");
+const yesBtn = document.querySelector("yesBtn");
+const noBtn = document.querySelector("noBtn");
+const dialogBox = document.querySelector("dialogBox");
+const popUp = document.querySelector("popUp");
