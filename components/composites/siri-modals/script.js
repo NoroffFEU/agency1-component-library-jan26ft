bcfd1edbb27d61a5739f2a89f4802a1490dd46a0
Siri-AnnNoroff <sirdoe07441@stud.noroff.no>
2026-09-22 09:04:26 +0200

    added eventlisteners, fixed bugs
    eventlisteners on all buttons, fixed bug with queryselector

--- components/composites/siri-modals/script.js
+++ components/composites/siri-modals/script.js
@@ -2,8 +2,31 @@
 
 //getting components
 
-const deleteBtn = document.querySelector("deleteBtn");
-const yesBtn = document.querySelector("yesBtn");
-const noBtn = document.querySelector("noBtn");
-const dialogBox = document.querySelector("dialogBox");
-const popUp = document.querySelector("popUp");
+const deleteBtn = document.querySelector("#deleteBtn");
+const yesBtn = document.querySelector("#yesBtn");
+const noBtn = document.querySelector("#noBtn");
+const dialogBox = document.querySelector("#dialogBox");
+const popUp = document.querySelector("#popUp");
+
+//delete button - hides button - shows dialogBox
+
+deleteBtn.addEventListener("click", function () {
+  deleteBtn.style.display = "none";
+  dialogBox.style.display = "flex";
+  yesBtn.style.display = "block";
+  noBtn.style.display = "block";
+});
+
+//yes button - hides dialogBox - shows popup
+
+yesBtn.addEventListener("click", function () {
+  dialogBox.style.display = "none";
+  popUp.style.display = "block";
+});
+
+// no button - hides dialogBox - shows delete button
+
+noBtn.addEventListener("click", function () {
+  dialogBox.style.display = "none";
+  deleteBtn.style.display = "inline-block";
+});
